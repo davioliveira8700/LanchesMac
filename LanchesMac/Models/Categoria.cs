5c7bf92d75ff0b6835d7b3f81c280e0579ed8014
Davi de Oliveira <odavi278@gmail.com>
2024-01-16 05:41:21 -0300

    Criando a string de conexao com banco de dados

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -5,5 +5,7 @@
         public int CategotiaId { get; set; }
         public string CategoriaNome { get; set; }
         public string CategoriaDescricao { get; set; }
+
+        public List<Lanche> Lanches{ get; set; }
     }
 }
--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,6 +1,20 @@
 ﻿namespace LanchesMac.Models
 {
-    public class Lanches
+    public class Lanche
     {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImageUrl { get; set; }
+        public string  ImageThumbnailUrl { get; set; }
+        public bool IsLanchesPreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
+
     }
 }
