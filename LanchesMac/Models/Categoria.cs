c755e0ec8794e2f1f74623a4df3398f9e8f472ff
Davi de Oliveira <odavi278@gmail.com>
2024-01-16 05:09:24 -0300

    Criando a classe lanche

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategotiaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string CategoriaDescricao { get; set; }
+    }
+}
